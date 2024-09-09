e44e8df9874f09db5e17e3fec3e3204173888974
solenwa <solen.wanono@gmail.com>
2024-09-09 17:13:38 +0200

    Template, frontend and backend initialized

---
+++ backend/data.js
@@ -0,0 +1,36 @@
+const data = {
+  products: [
+    {
+      _id: '1',
+      name: 'Slim Shirt',
+      category: 'Shirts',
+      image: '/images/d1.jpeg',
+      price: 60,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      _id: '2',
+      name: 'Fit Shirt',
+      category: 'Shirts',
+      image: '/images/d1.jpeg',
+      price: 50,
+      brand: 'Nike',
+      rating: 4.2,
+      numReviews: 10,
+    },
+    {
+      _id: '3',
+      name: 'Best Pants',
+      category: 'Pants',
+      image: '/images/d1.jpeg',
+      price: 55,
+      brand: 'Nike',
+      rating: 4.7,
+      numReviews: 10,
+    },
+  ],
+};
+
+export default data;
