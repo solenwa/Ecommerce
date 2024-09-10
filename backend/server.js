054503a8485b93dfaf5fb29cabffa54913e14f60
solenwa <solen.wanono@gmail.com>
2024-09-10 17:30:49 +0200

    Store logic added, bug remaining

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,16 @@ app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
 
+app.get('/api/products/:id', (req, res) => {
+  const productId = req.params.id;
+  res.send(data.products.find((x) => x._id === productId));
+  if(product){
+    res.send(product);
+  } else {
+    res.status(404).send({msg: "Product Not Found"})
+  }
+});
+
 app.listen(5000, () => {
   console.log('Server started at http://localhost:5000');
 });
